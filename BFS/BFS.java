d3dad5b4582b9998d281291ebbf54cbe953e9773
Taleb <alitk@vt.edu>
2020-05-12 19:58:31 -0400

    Change the structure for VS code

--- BFS/BFS.java
+++ BFS/BFS.java
@@ -36,20 +36,19 @@ public class Main {
   public static class Graph{
    
       private int V;   // No. of vertices 
-      private LinkedList<Integer> adj[]; // Adjacency List Represntation 
+      private ArrayList<ArrayList<Integer>> adj; // Adjacency List Represntation 
     
       // Constructor 
       Graph(int v) { 
           V = v; 
-          adj = new LinkedList[v]; 
-          for(int i=0; i<v; ++i) 
-              adj[i] = new LinkedList(); 
+          adj  = new ArrayList<ArrayList<Integer> >(V); 
+          
       } 
     
       // Function to add an edge into the graph 
       void addEdge(int v,int w) { 
-          adj[v].add(w); 
-          adj[w].add(v); 
+          adj.get(v).add(w); 
+          adj.get(w).add(v); 
       } 
 
       public ArrayList<Integer> BFS(int s){
